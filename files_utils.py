70033a42caf2904346dc92681a3f0880ca225e7b
Kirill863 <Byblik4310@gmail.com>
2024-12-23 17:52:42 +0300

    добавил функцию write_json

--- files_utils.py
+++ files_utils.py
@@ -6,3 +6,7 @@ def read_json(file_path: str, encoding: str = "utf-8"):
         data = json.load(file)
     return data
 
+def write_json(data, file_path: str, encoding: str = "utf-8"):
+    """Записывает данные в JSON-файл."""
+    with open(file_path, 'w', encoding=encoding) as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
